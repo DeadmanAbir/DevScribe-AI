5806774328764e44aedc14a23971952949b23d9e
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-23 17:06:44 +0530

    added : shadcnUI the project

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <Button>Connect</Button>
+    </main>
+  );
+}
