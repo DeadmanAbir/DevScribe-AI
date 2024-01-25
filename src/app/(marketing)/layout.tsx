9edc967a95cfa96e8e646b79eaaa61a562e0f8bf
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-25 21:17:29 +0530

    added : faq section added

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -2,7 +2,7 @@ import Navbar from './_components/navbar'
 
 const MarketingPageLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full  bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-indigo-950 to-black">
+    <div className="h-full   bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-indigo-950 to-black">
       <div className="flex w-full items-center justify-center">
         <Navbar />
       </div>
