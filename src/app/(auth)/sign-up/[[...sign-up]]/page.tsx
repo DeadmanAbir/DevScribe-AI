a583f1998980070f0a46a77830cb32fc676860ce
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-01-27 12:53:22 +0530

    fix : back to home button added

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,17 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignUp } from '@clerk/nextjs'
+import { ArrowLeft } from 'lucide-react'
+import Link from 'next/link'
 
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-950 to-black">
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-indigo-950 to-black">
+      <Link href="/">
+        <div className="text-white w-96 flex pb-3 hover:text-white/80">
+          <ArrowLeft />
+          Back to home
+        </div>
+      </Link>
+
       <SignUp />
     </div>
   )
