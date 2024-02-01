6ba8f0a3c2506f34a88565405acfbda084a83e39
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-02-01 20:41:09 +0530

    added : create folder added

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,13 +1,13 @@
 import Navbar from '../_components/navbar'
 import DashboardSidebar from '../_components/sidebar'
 
-
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <Navbar />
       <div className="flex h-screen overflow-y-auto bg-gradient-to-b from-indigo-950 to-black ">
         <DashboardSidebar />
+
         {children}
       </div>
     </>
