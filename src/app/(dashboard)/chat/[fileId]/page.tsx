fcef91194e6582d3d669e3cf9c7446e65f034642
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2024-02-12 11:57:24 +0530

    fix : skeleton fix , icons fixed

--- src/app/(dashboard)/chat/[fileId]/page.tsx
+++ src/app/(dashboard)/chat/[fileId]/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "../_components/navbar";
 const Interaction = ({params} : any) => {
     console.log(params);
     //now fetch all detasils about the file and then proceed
-    return ( <div className="h-screen overflow-hidden max-w-7xl mx-auto">
+    return ( <div className="h-screen overflow-hidden max-w-full mx-auto">
         <Navbar/>
         <MainScreen/>
     </div> );
