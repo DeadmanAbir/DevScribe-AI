51f83aebe7372ade54be658d294fa06886afc888
Your Name <abirdutta90990@gmail.com>
2024-02-25 21:40:05 +0530

    Fixes

--- src/server/api/routers/Files/file.router.ts
+++ src/server/api/routers/Files/file.router.ts
@@ -3,6 +3,8 @@ import { getFiles } from './getFiles';
 import { deleteFile } from './deleteFile';
 import { createTRPCRouter } from '../../trpc';
 
+export const maxDuration = 300;
+
 export const fileRouter = createTRPCRouter({
 	createFile,
     getFiles,
